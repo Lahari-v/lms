2e69c33d6c32865c99446722f45691918c4b3056
Lahari-v <lahariv28@gmail.com>
2025-06-13 20:32:40 +0530

    fix: correct header mapping in webhook verification

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -8,7 +8,8 @@ export const clerkWebhooks = async (req, res)=>{
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
         await whook.verify(JSON.stringify(req.body), {
-            'svix-id': req.headers['svix-timestamp'],
+            'svix-id': req.headers['svix-id'],
+            'svix-timestamp': req.headers['svix-timestamp'],
             'svix-timestamp': req.headers['svix-signature']
         })
 
