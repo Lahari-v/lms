59e84da4a05b4e06d615868bada2ff7601fa13c5
Lahari-v <lahariv28@gmail.com>
2025-06-14 21:54:10 +0530

    feat: implement course management and payment processing with Stripe integration

--- server/server.js
+++ server/server.js
@@ -1,58 +1,38 @@
-// import express from 'express'
-// import cors from 'cors'
-// import 'dotenv/config' 
-// import connectDB from './configs/mongodb.js'
-// import { clerkWebhooks } from './controllers/webhooks.js'
-
-
-// //Initialize Express
-// const app = express()
-
-// //Connect to database
-// await connectDB()
-
-// //Middleware
-// app.use(cors())
-
-// //Routes
-// app.get('/', (req, res)=> res.send("API Working"))
-// app.post('/clerk', express.json(), clerkWebhooks)
-
-// //Port
-// const PORT = process.env.PORT || 5000
-
-// app.listen(PORT, ()=>{
-//     console.log(`Server is running on port ${PORT}`)
-// })
-
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-import { clerkWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import educatorRouter from './routes/educatorRoutes.js'
+import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 // Initialize Express
 const app = express()
 
 // Connect to MongoDB
 await connectDB()
+await connectCloudinary()
 
-// CORS middleware
+// Middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 
-// Clerk webhook must comze before express.json() for raw body
-app.post('/clerk', express.raw({ type: '*/*' }), clerkWebhooks)
-
-// Normal JSON body parser for other routes
-app.use(express.json())
-
-// Route for testing
+// Routes
+app.post('/clerk', express.raw({ type: '*/*' }), clerkWebhooks) 
+app.use(express.json()) 
 app.get('/', (req, res) => {
   res.send("API Working")
 })
+app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter)
+app.use('/api/user', express.json(), userRouter) 
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
-// Start server
+// Port
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
